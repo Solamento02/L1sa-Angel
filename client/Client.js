6a2ff8f0e8b49af89d2d3237843e6b4d6de9e0a9
Noah Vogel <103073069+Solamento02@users.noreply.github.com>
2023-01-07 01:18:59 -0300

    Update Client.js

--- client/Client.js
+++ client/Client.js
@@ -1,9 +1,9 @@
-const {Client, Collection, GatewayIntentBits, Partials} = require('discord.js');
+const {Client, Collection, Intents } = require('discord.js');
 
 module.exports = class extends Client {
   constructor(config) {
     super({
-      intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+      intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS,],
     });
 
     this.commands = new Collection();
