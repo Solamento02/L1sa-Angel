b6ca91e41b0fcd0597b1b7d1e8b31e0d4d998fde
Noah Vogel <103073069+Solamento02@users.noreply.github.com>
2023-01-07 01:29:11 -0300

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -6,17 +6,17 @@ module.exports = {
   async execute(interaction, player) {
     if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
       return void interaction.reply({
-        content: 'You are not in a voice channel!',
+        content: 'Você não está em um canal de voz!!😼!',
         ephemeral: true,
       });
     }
 
     if (
-      interaction.guild.members.me.voice.channelId &&
-      interaction.member.voice.channelId !== interaction.guild.members.me.voice.channelId
+      interaction.guild.me.voice.channelId &&
+      interaction.member.voice.channelId !== interaction.guild.me.voice.channelId
     ) {
       return void interaction.reply({
-        content: 'You are not in my voice channel!',
+        content: 'Você não está em um canal de voz!!😼!',
         ephemeral: true,
       });
     }
@@ -25,7 +25,7 @@ module.exports = {
     const queue = player.getQueue(interaction.guildId);
     if (!queue || !queue.playing)
       return void interaction.followUp({
-        content: '❌ | No music is being played!',
+        content: '❌ | Sem músicas tocando! ',
       });
     const progress = queue.createProgressBar();
     const perc = queue.getPlayerTimestamp();
@@ -33,7 +33,7 @@ module.exports = {
     return void interaction.followUp({
       embeds: [
         {
-          title: 'Now Playing',
+          title: 'Agora tocando..🎶😸',
           description: `🎶 | **${queue.current.title}**! (\`${perc.progress}%\`)`,
           fields: [
             {
